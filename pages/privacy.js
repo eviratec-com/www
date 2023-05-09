d070287023ecddeef7a8451370ab46b8a169ae39
Callan Peter Milne <callanpmilne@gmail.com>
2023-05-09 13:18:59 +1000

    Added Pages: Privacy Policy; and Terms of Use

---
+++ pages/privacy.js
@@ -0,0 +1,176 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import Image from 'next/image'
+import styles from '@/styles/Legal.module.css'
+
+import Footer from '@/components/Footer'
+
+export default function PrivacyPolicyPage() {
+  return (
+    <>
+      <Head>
+        <title>Privacy Policy - Eviratec</title>
+        <meta name="description" content="Eviratec Privacy Policy" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="theme-color" content="rgba(33,33,33,1)" />
+        <meta property="og:title" content="Eviratec Privacy Policy" />
+        <meta property="og:description" content="Eviratec Privacy Policy" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.eviratec.com.au/privacy" />
+        <meta property="og:image" content="https://www.eviratec.com.au/og.png" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1>Privacy Policy</h1>
+
+        <p>Last updated: April 20, 2023</p>
+
+        <section className={styles.legal}>
+          <p>EVIRATEC.COM (&quot;us&quot;, &quot;we&quot;, or &quot;our&quot;) operates the <Link href="https://www.eviratec.com.au">https://www.eviratec.com.au</Link> website (the &quot;Service&quot;).</p>
+
+          <p>This page informs you of our policies regarding the collection, use and disclosure of Personal Information when you use our Service.</p>
+
+          <p>We will not use or share your information with anyone except as described in this Privacy Policy.</p>
+
+          <p>We use your Personal Information for providing and improving the Service. By using the Service, you agree to the collection and use of information in accordance with this policy. Unless otherwise defined in this Privacy Policy, terms used in this Privacy Policy have the same meanings as in our Terms of Use, accessible at <Link href="https://www.eviratec.com.au/terms">https://www.eviratec.com.au/terms</Link>.</p>
+
+
+
+          <h2>Information Collection And Use</h2>
+
+          <p>While using our Service, we may ask you to provide us with certain personally identifiable information that can be used to contact or identify you. Personally identifiable information may include, but is not limited to, your email address, name, other information (&quot;Personal Information&quot;).</p>
+
+          <p>The purpose for which we collect personal information is to provide you with the best service experience possible on the Service and for our internal business purposes that form part of normal business practices. Some provision of personal information is optional. However, if you do not provide us with certain types of personal information, you may be unable to enjoy the full functionality of the Service.</p>
+
+
+          <h2>Log Data</h2>
+
+          <p>We may also collect information that your browser sends whenever you visit our Service (&quot;Log Data&quot;). This Log Data may include information such as your computer&apos;s Internet Protocol (&quot;IP&quot;) address, browser type, browser version, the pages of our Service that you visit, the time and date of your visit, the time spent on those pages and other statistics.</p>
+
+          <p>In addition, we may use third party services such as Google Analytics that collect, monitor and analyze this type of information in order to increase our Service&apos;s functionality. These third party service providers have their own privacy policies addressing how they use such information.</p>
+
+
+
+
+          <h2>Cookies</h2>
+
+          <p>Cookies are files with small amount of data, which may include an anonymous unique identifier. Cookies are sent to your browser from a web site and stored on your computer&apos;s hard drive.</p>
+
+          <p>We use &quot;cookies&quot; to collect information. You can instruct your browser to refuse all cookies or to indicate when a cookie is being sent. However, if you do not accept cookies, you may not be able to use some portions of our Service.</p>
+
+          <p>We send a session cookie to your computer when you log in to your User account. This type of cookie helps if you visit multiple pages on the Service during the same session, so that you don&apos;t need to enter your password on each page. Once you log out or close your browser, this cookie expires.</p>
+
+          <p>We also use longer-lasting cookies for other purposes such as to display your Content and account information. We encode our cookie so that only we can interpret the information stored in them. Users always have the option of disabling cookies via their browser preferences. If you disable cookies on your browser, please note that some parts of our Service may not function as effectively or may be considerably slower.</p>
+
+
+
+
+
+
+          <h2>Service Providers</h2>
+
+          <p>We may employ third party companies and individuals to facilitate our Service, to provide the Service on our behalf, to perform Service-related services or to assist us in analyzing how our Service is used.</p>
+
+          <p>These third parties have access to your Personal Information only to perform these tasks on our behalf and are obligated not to disclose or use it for any other purpose.</p>
+
+
+
+
+          <h2>Compliance With Laws</h2>
+
+          <p>We may disclose personal information in special situations where we have reason to believe that doing so is necessary to identify, contact or bring legal action against anyone damaging, injuring or interfering (intentionally or unintentionally) with our rights or property, users or anyone else who could be harmed by such activities.</p>
+
+          <p>We will disclose your Personal Information where required to do so by law or subpoena or if we believe that such action is necessary to comply with the law and the reasonable requests of law enforcement or to protect the security or integrity of our Service.</p>
+
+
+          <h2>Business Transaction</h2>
+
+          <p>In the event that we sell or buy businesses or their assets, or engage in transfers, acquisitions, mergers, restructurings, changes of control and other similar transactions, customer or user information is generally one of the transferable business assets. Thus, your personal information may be subject to such a transfer. In the unlikely event of insolvency, personal information may be transferred to a trustee or debtor in possession and then to a subsequent purchaser.</p>
+
+
+          <h2>Security</h2>
+
+          <p>The security of your Personal Information is important to us, but remember that no method of transmission over the Internet, or method of electronic storage is 100% secure. While we strive to use commercially acceptable means to protect your Personal Information, we cannot guarantee its absolute security.</p>
+
+
+          <h2>International Transfer</h2>
+
+          <p>Your information, including Personal Information, may be transferred to — and maintained on — computers located outside of your state, province, country or other governmental jurisdiction where the data protection laws may differ than those from your jurisdiction.</p>
+
+          <p>If you are located outside Australia and choose to provide information to us, please note that we transfer the information, including Personal Information, to Australia and process it there.</p>
+
+          <p>Your consent to this Privacy Policy followed by your submission of such information represents your agreement to that transfer.</p>
+
+
+          <h2>Access and Correction</h2>
+
+          <p>Australian Privacy Principle 6 of the Privacy Act 1988 (Cth) allows you to get access to, and correct, the personal information we hold about you in certain circumstances. If you would like to obtain such access, please contact us on the details set out above.</p>
+
+          <p>Please note that the access and correction requirements under this Privacy Policy operates alongside and do not replace other informal or legal procedures by which an individual can be provided access to, or correction of, their personal information, including the requirements under the Freedom of Information Act 1982 (Cth).</p>
+
+
+          <h2>Complaints</h2>
+
+          <p>Australian Privacy Principle 1 of the Privacy Act 1988 (Cth) allows you to make a complaint about any alleged breaches of privacy. In order to lodge a complaint with us, please contact us using the details above with the following information:</p>
+
+          <ul>
+            <li>
+              Your name and address;
+            </li>
+            <li>
+              Details of the alleged breach of privacy; and
+            </li>
+            <li>
+              URL link to the alleged breach of privacy (if applicable).
+            </li>
+          </ul>
+
+          <p>Please allow us 30 days to investigate your complaint, after which we will contact you immediately to resolve the issue.</p>
+
+
+          <h2>Retention of Information</h2>
+
+          <p>We retain information for as long as required, allowed or we believe it useful, but do not undertake retention obligations. We may dispose of information in our discretion without notice, subject to applicable law that specifically requires the handling or retention of information. You must keep your own, separate back-up records.</p>
+
+
+          <h2>Links To Other Sites</h2>
+
+          <p>Our Service may contain links to other sites that are not operated by us. If you click on a third party link, you will be directed to that third party&apos;s site. We strongly advise you to review the Privacy Policy of every site you visit.</p>
+
+          <p>We have no control over, and assume no responsibility for the content, privacy policies or practices of any third party sites or services.</p>
+
+
+          <h2>Children&apos;s Privacy</h2>
+
+          <p>Our Service does not address anyone under the age of 18 (&quot;Children&quot;).</p>
+
+          <p>We do not knowingly collect personally identifiable information from children under 18. If you are a parent or guardian and you are aware that your Children has provided us with Personal Information, please contact us. If we become aware that we have collected Personal Information from children under 18 without verification of parental consent, we take steps to remove that information from our servers or replace it with the Personal Information of the Children’s parent or guardian.</p>
+
+
+          <h2>Changes To This Privacy Policy</h2>
+
+          <p>We may update our Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on this page.</p>
+
+          <p>You are advised to review this Privacy Policy periodically for any changes. Changes to this Privacy Policy are effective when they are posted on this page.</p>
+
+          <p>If we make any material changes to this Privacy Policy, we will notify you either through the email address you have provided us, or by placing a prominent notice on our website.</p>
+
+
+          <h2>Consent</h2>
+
+          <p>You warrant that you are able to give consents under Australian Law or, in the event that you do not have the capacity to give consent, you warrant that your guardian or attorney is able to give any consent required under this Privacy Policy on your behalf.</p>
+
+          <p>You hereby expressly and voluntarily grant your informed consent to us to deal with your personal information in accordance with the terms and conditions of this Privacy Policy. Should you retract your consent, please contact us. If you retract your consent, you acknowledge and agree that failure to provide certain types of personal information may not give you access to the full functionality of the Service.</p>
+
+
+          <h2>Contact Us</h2>
+
+          <p>If you have any questions about this Privacy Policy, please contact us via email: <Link href="mailto:legal@eviratec.com.au">legal@eviratec.com.au</Link>.</p>
+        </section>
+      </main>
+
+      <Footer />
+    </>
+  )
+}
