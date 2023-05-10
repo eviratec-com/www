03d196491b6434e682530d647a15cb357ec84d0f
Callan Peter Milne <callanpmilne@gmail.com>
2023-05-10 16:12:12 +1000

    Added Button Component

---
+++ components/Button/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import type { ReactDomElement } from 'react-dom'
+import Link from 'next/link'
+
+import styles from './Button.module.css'
+
+interface Props {
+  children?: ReactDomElement | ReactDomElement[]
+  href?: string
+  onClick?: (event: any) => void
+  className?: string
+}
+
+export default function Button({ children, href, onClick, className }: Props) {
+  return (
+    <>
+      {!href &&
+        <div
+          className={`${styles._} ${className ? className : ''}`}
+          onClick={onClick}
+        >
+          {children}
+        </div>
+      ||
+        <div className={`${styles._} ${className ? className : ''}`}>
+          <Link
+            href={href}
+            onClick={onClick}
+            scroll={false}
+            prefetch={false}
+          >
+            {children}
+          </Link>
+        </div>
+      }
+    </>
+  )
+}
