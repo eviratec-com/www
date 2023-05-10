2df219e3c82f7dd92fba163bb0e4799f514e97a9
Callan Peter Milne <callanpmilne@gmail.com>
2023-05-10 16:12:50 +1000

    Updated db to use connection pools

--- db.js
+++ db.js
@@ -1,5 +1,11 @@
 const { Pool, Client } = require('pg')
 const connectionString = process.env.DB_URI
+//
+// export default function dbClient() {
+//   return new Client({
+//     connectionString: process.env.DB_URI,
+//   })
+// }
 
 // const pool = new Pool({
 //   connectionString,
@@ -10,8 +16,21 @@ const connectionString = process.env.DB_URI
 //   pool.end()
 // })
 
-export default function dbClient() {
-  return new Client({
-    connectionString: process.env.DB_URI,
-  })
+// const { Pool } = require('pg')
+
+const pool = new Pool({
+  connectionString,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+})
+
+export default async function dbClient() {
+  // check out a single client
+  const client = await pool.connect()
+  return client
+}
+
+export function endPool() {
+  return pool.end()
 }
