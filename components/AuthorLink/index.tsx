6ab63522ac903804651245086bb2eabe1323244f
Callan Peter Milne <callanpmilne@gmail.com>
2023-05-10 16:12:25 +1000

    Updated AuthorLink Component

--- components/AuthorLink/index.tsx
+++ components/AuthorLink/index.tsx
@@ -7,12 +7,19 @@ import styles from './AuthorLink.module.css'
 
 export interface Props {
   author: UserProfile
+  prefix?: string
 }
 
-export default function AuthorLink({ author }: Props) {
+export default function AuthorLink({ author, prefix }: Props) {
+  const [textBefore, setTextBefore] = useState<string>(prefix || '')
+
+  useEffect(() => {
+    setTextBefore(prefix || '')
+  }, [prefix])
+
   return (
     <address className={styles._}>
-      By <Link href={author.link}>{author.display_name}</Link>
+      {textBefore} <Link prefetch={false} href={author.link}>{author.display_name}</Link>
     </address>
   )
 }
