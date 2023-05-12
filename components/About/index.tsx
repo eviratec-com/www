182305614eba1db88be98a9060b3c10a53e95af6
Callan Peter Milne <callanpmilne@gmail.com>
2023-05-12 23:58:34 +1000

    Updated About Component Text and Style

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -8,44 +8,48 @@ export default function About() {
   return (
     <div className={styles._}>
       <p>
-        My name is Callan Milne. I’m a full-stack web development expert from
-        Australia. I have been developing full-stack websites and web
-        applications full-time since 2007. I started developing single-page
-        applications (SPAs), and web APIs in 2011.
+        We&apos;re an Australian web development business, specialising in
+        full-stack web applications, and software systems. We provide a number
+        of services and products, including a white-label Social Network
+        solution.
       </p>
 
       <p>
-        I love science, video games, computers, and programming; and was
-        a hobbyist programmer before I started working as a PHP Developer in
-        March 2007.
+        <Link href="mailto:info@eviratec.com">Contact us</Link> for more
+        information, about our professional web application design, development,
+        and devops services.
+      </p>
+
+      <h3>What we do</h3>
+
+      <p>
+        We create websites, web apps, native apps, web apis, and micro-services;
+        with extensive experience around high-availability multimedia uploads,
+        and processing. We have experience creating photo upload and processing
+        solutions in Microsoft Azure, and Amazon Web Services (AWS).
       </p>
 
       <p>
-        I <em>first</em> started working with software engineering tools in 1998; My
-        first programming experience was with Microsoft Visual Basic, and C++.
-        I started working with web technology in 2001, and published my first
-        PHP-Nuke CMS powered website in 2003.
+        We love building serverless functions, aka: AWS Lambdas; or Azure App
+        Functions.
       </p>
 
       <p>
-        Since then, I have created numerous web applications, user interfaces,
-        web APIs, and software packages. I love to contribute to open source
-        software. My first public contribution was published on SourceForge
-        in 2006. I now keep my public contributions on <Link href="https://github.com/eviratec">GitHub</Link>,
-        and I have packages published on <Link href="https://www.npmjs.com/~luminous-patterns">NPM</Link>.
+        We also provide general website and app development services, including:
+        updating pages, and websites.
       </p>
 
-      <p>
-        I like to keep my skills up-to-date, and have extensive experience with
-        state-of-the-art web development technologies including: Next.js,
-        React.js, TypeScript, Cypress, Jest, Storybook, and AWS.
-      </p>
+      <h3>Preferred Technology</h3>
 
-      <p>
-        I am passionate about test automation, and test driven development
-        (TDD), with strong experience in developing tests using Node.js,
-        Jasmine, Cypress, and Jest.
-      </p>
+      <p>We prefer to use the following technologies:</p>
+
+      <ul>
+        <li>React.js (18)</li>
+        <li>Next.js (13)</li>
+        <li>TypeScript</li>
+        <li>Node.js</li>
+        <li>Microsoft Azure</li>
+      </ul>
     </div>
   )
 }
